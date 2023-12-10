807b52791d4109cdcdf8ccffb2f79ad8a9a19a6a
Bode <bode.falade@outlook.com>
2023-12-10 16:48:05 -0600

    added provider and varibles tf.
    added main file, variable and provider tf files.

---
+++ provider.tf
@@ -0,0 +1,13 @@
+// Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,4 @@
+// Configure the AWS Provider, specify region where the resources will be created.
+provider "aws" {
+  region = "us-east-1"
+}
